1a4890b6c8a7f316d499887e1345e51759551dfc
SukaKode <gsukakode@gmail.com>
2021-05-19 00:16:21 +0700

    Let's Code Master ~

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('main') }}" class="brand-link">
       <img src="{{ asset('assets') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"></span>
+      <span class="brand-text font-weight-light">{{ env("APP_NAME") }}</span>
     </a>
 
     <div class="sidebar ">
